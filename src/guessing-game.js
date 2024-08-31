e2930bfefafb545635f518d8f65da311bfe0bb97
Andrii <andriipigal@gmail.com>
2024-08-31 16:18:22 +0200

    feat: first version

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,26 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.ans = this.min + Math.floor((this.max - this.min) / 2);
+        return this.ans;
     }
 
     lower() {
-
+        this.max = this.ans;
     }
 
     greater() {
-
+        this.min = this.ans;
     }
+
+    min = null;
+    max = null;
+    ans = null;
 }
 
 module.exports = GuessingGame;
